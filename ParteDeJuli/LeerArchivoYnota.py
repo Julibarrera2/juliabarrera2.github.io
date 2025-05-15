545bae1d135173ed5a7258557521b1e62aee0a36
Julibarrera2 <164010054+Julibarrera2@users.noreply.github.com>
2025-05-15 14:31:42 -0300

    nose algo

---
+++ ParteDeJuli/LeerArchivoYnota.py
@@ -0,0 +1,12 @@
+# Importación de librerías
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+import soundfile as sf
+import scipy
+import os
+from pydub import AudioSegment
+#Importamos madmom
+from madmom.audio.onsets import OnsetPeakPickingProcessor, OnsetDetection
+from madmom.features.beats import RNNBeatProcessor, DBNBeatTrackingProcessor
